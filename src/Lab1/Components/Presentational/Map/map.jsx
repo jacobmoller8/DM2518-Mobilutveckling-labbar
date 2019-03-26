af50cfe83a6e48de34ea11301a87d3a72d1c76cf
hugo bergqvist <hugobergqvist@hugos-MBP.lan>
2019-03-26 16:22:45 +0100

    added some btn styling

--- src/Lab1/Components/Presentational/Map/map.jsx
+++ src/Lab1/Components/Presentational/Map/map.jsx
@@ -6,10 +6,10 @@ import './map.css'
 export default class Map extends Component {
     render() {
         let tiltBtn = null
-        if (this.props.tiltVisibility){
-            tiltBtn = <Button className="mapTypeBtn" onClick={() => this.props.onTiltClick()}> Tilt Map</Button>
-        }else{
-            tiltBtn = <Button className="mapTypeBtn" disabled onClick={() => this.props.onTiltClick()}> Tilt Map</Button>
+        if (this.props.tiltVisibility) {
+            tiltBtn = <Button className="tiltBtn" onClick={() => this.props.onTiltClick()}> Tilt Map</Button>
+        } else {
+            tiltBtn = <Button className="tiltBtn" disabled onClick={() => this.props.onTiltClick()}> Tilt Map</Button>
         }
 
 
@@ -18,20 +18,27 @@ export default class Map extends Component {
                 <div className="container-fluid header">
                     <div className="row justify-content-center">
                         <h3 id="headerTitle"> Our cool map</h3>
-                        {tiltBtn}
                     </div>
                 </div>
                 <div className="mapContainer">
                     <div id="map"></div>
                 </div>
-                <div className="container-fluid col-12 col-md-10  mapTypeBtnContainer">
-                    <div className="row justify-content-center">
+                <div className="container-fluid col-sm-12 col-md-10 col-lg-8 btnContainer">
+                    <div className="row">
+                    <h5 className="btnRowTitle ">Map type buttons: </h5>
                         <Button className="mapTypeBtn" onClick={() => this.props.onMaptypeClick('satellite')}>Satellite</Button>
                         <Button className="mapTypeBtn" onClick={() => this.props.onMaptypeClick('hybrid')}>Hybrid</Button>
                         <Button className="mapTypeBtn" onClick={() => this.props.onMaptypeClick('roadmap')}>Roadmap</Button>
                         <Button className="mapTypeBtn" onClick={() => this.props.onMaptypeClick('terrain')}>Terrain</Button>
+                    </div>
+                    <div className="row">
+                    <h5 className="btnRowTitle">Zoom buttons: </h5>
                         <Button className="zoomBtn" onClick={() => this.props.onZoomClick(1)}>+</Button>
                         <Button className="zoomBtn" onClick={() => this.props.onZoomClick(-1)}>-</Button>
+                    </div>
+                    <div className="row">
+                    <h5 className="btnRowTitle">Function buttons: </h5>
+                        {tiltBtn}
                     </div>
                 </div>
             </div>
