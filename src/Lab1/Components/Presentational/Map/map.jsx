3739903e35b151384748578a3d0855da4a78ea04
hugo bergqvist <hugobergqvist@hugos-MBP.lan>
2019-03-26 14:10:05 +0100

    added style to btns

--- src/Lab1/Components/Presentational/Map/map.jsx
+++ src/Lab1/Components/Presentational/Map/map.jsx
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
-import {Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 import './map.css'
 
 export default class Map extends Component {
     render() {
         return (
-            <React.Fragment>
+            <div className="container-fluid col-12">
                 <div className="container-fluid header">
                     <h3 id="headerTitle"> Our cool map</h3>
                 </div>
                 <div className="mapContainer">
                     <div id="map"></div>
                 </div>
-                <Button onClick={() => this.props.onMaptypeClick('satellite')}>Satellite</Button>
-                <Button onClick={() => this.props.onMaptypeClick('hybrid')}>hybrid</Button>
-                <Button onClick={() => this.props.onMaptypeClick('roadmap')}>roadmap</Button>
-                <Button onClick={() => this.props.onMaptypeClick('terrain')}>terrain</Button>
-            </React.Fragment>
+                <div className="container-fluid col-8 mapTypeBtnContainer">
+                <div class="row justify-content-center">
+                    <Button className="mapTypeBtn" onClick={() => this.props.onMaptypeClick('satellite')}>Satellite</Button>
+                    <Button className="mapTypeBtn" onClick={() => this.props.onMaptypeClick('hybrid')}>hybrid</Button>
+                    <Button className="mapTypeBtn" onClick={() => this.props.onMaptypeClick('roadmap')}>roadmap</Button>
+                    <Button className="mapTypeBtn" onClick={() => this.props.onMaptypeClick('terrain')}>terrain</Button>
+                </div>
+                </div>
+            </div>
         )
     }
 }
